0c96958f1b0c3864f72df505d38e62824ce53169
ShipJH <ship_reappearance@naver.com>
2019-07-20 18:20:36 +0900

    재현 뼈대생성

---
+++ src/components/Page.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="pageArea">
+    이전 1 2 3 4 5 6 7 8 9 10 다음
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageArea"
+};
+</script>
+
+<style scoped>
+.pageArea {
+  border: 1px red solid;
+}
+</style>
