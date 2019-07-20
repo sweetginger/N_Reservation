66f72410d1475ea040e7a885015cc960f956eaef
ShipJH <ship_reappearance@naver.com>
2019-07-20 18:09:09 +0900

    배재현 기본 컴포넌트 생성

---
+++ src/components/Shop/ItemQty.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="itemQtyArea">
+    <div class="itemQty">상품선택,수량</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemQtyArea"
+};
+</script>
+
+<style scoped>
+.itemQty {
+  border: 1px darkblue solid;
+}
+</style>
---
+++ src/components/Shop/ShopLocation.vue
@@ -0,0 +1,30 @@
+<template>
+  <!--재현-->
+  <div class="locationArea">
+    <div class="locationMap">지도지도</div>
+    <div class="locationInfo">위치정보정보</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LocationArea"
+};
+</script>
+
+<style scoped>
+.locationArea {
+  border: 1px red solid;
+  display: flex;
+}
+
+.locationMap {
+  border: 1px green solid;
+  width: 50%;
+}
+
+.locationInfo {
+  border: 1px blue solid;
+  width: 50%;
+}
+</style>
