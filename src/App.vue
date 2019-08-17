2c943292d40905cb45091357e506e18ca6875733
sweetginger <hyr2902@gmail.com>
2019-08-17 13:13:01 +0900

    router link 변경

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,6 @@
   <div id="app">
     <header>header영역</header>
     <div id="nav">
-      <!-- <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>|
-      <router-link to="/shop">Shop</router-link>|
-      <router-link to="/yurim">유림</router-link>|
-      <router-link to="/joo">joo</router-link>| -->
       <router-link to="/">홈</router-link>
       <router-link to="/shop">매장뷰</router-link>
       <router-link to="/shopOption">매장옵션뷰</router-link>
